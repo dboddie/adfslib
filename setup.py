547df85612ca08738417dae5888116bcae0bd8f8
David Boddie <david@boddie.org.uk>
2003-03-26 18:52:31 +0100

    Adding distutils specific files.

---
+++ setup.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name="ADF2INF",
+    description="ADFS disk image reader",
+    author="David Boddie",
+    author_email="david@boddie.org.uk",
+    url="http://www.boddie.org.uk/david/Projects/Python/ADF2INF",
+    version="0.31c",
+    py_modules=["ADF2INF"]
+    )
