177971f2d102ea95e38adbb26fffa192a473c87f
David Boddie <david@boddie.org.uk>
2003-03-27 01:26:20 +0100

    Split the ADF2INF.py file into two: ADFSlib.py (containing the ADFSdisc class) and ADF2INF.py (a command line utility).

--- setup.py
+++ setup.py
@@ -1,13 +1,28 @@
 #! /usr/bin/env python
 
 from distutils.core import setup
+import sys
+
+try:
+
+    import cmdsyntax
+
+except ImportError:
+
+    print "Apologies:"
+    print
+    print "You need the cmdsyntax module to be able to use this tool."
+    print "See http://www.boddie.org.uk/david/Projects/Python/CMDSyntax"
+    print "for details."
+    sys.exit()
 
 setup(
-    name="ADF2INF",
-    description="ADFS disk image reader",
+    name="ADFSlib",
+    description="ADFS disk image reader and utility",
     author="David Boddie",
     author_email="david@boddie.org.uk",
     url="http://www.boddie.org.uk/david/Projects/Python/ADF2INF",
     version="0.31c",
-    py_modules=["ADF2INF"]
+    py_modules=["ADFSlib"]
+    scripts=["ADF2INF"]
     )
