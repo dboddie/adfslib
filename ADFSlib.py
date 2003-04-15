6586d03372e208aefd36ac6cb70725b1e4b0d6ed
David Boddie <david@boddie.org.uk>
2003-04-15 23:42:23 +0200

    Updated the version number and date.

--- ADFSlib.py
+++ ADFSlib.py
@@ -6,8 +6,8 @@ A library for reading ADFS disc images.
 """
 
 __author__ = "David Boddie <david@boddie.org.uk>"
-__date__ = "Sat 29th March 2003"
-__version__ = "0.10"
+__date__ = "Tue 15th April 2003"
+__version__ = "0.11"
 
 
 import os, string
