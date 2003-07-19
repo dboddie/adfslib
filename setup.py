8ea482ca1e1a82edca609a41364f33813988ac2d
David Boddie <david@boddie.org.uk>
2003-07-20 00:26:12 +0200

    Updated the version number.

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
     author="David Boddie",
     author_email="david@boddie.org.uk",
     url="http://www.boddie.org.uk/david/Projects/Python/ADFSlib/",
-    version="0.14",
+    version="0.20",
     py_modules=["ADFSlib"],
     scripts=["ADF2INF.py"]
     )
