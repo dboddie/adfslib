6b4492d69010f21344be9941fd949c3ec3478b36
David Boddie <david@boddie.org.uk>
2008-07-13 00:04:56 +0200

    Large update to version number to indicate the license upgrade to GPL v3.

--- ADFSlib.py
+++ ADFSlib.py
@@ -6,26 +6,24 @@ A library for reading ADFS disc images.
 
 Copyright (c) 2003-2008, David Boddie
 
-This software is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License as
-published by the Free Software Foundation; either version 2 of
-the License, or (at your option) any later version.
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
 
-This software is distributed in the hope that it will be useful,
+This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public
-License along with this library; see the file COPYING
-If not, write to the Free Software Foundation, Inc.,
-59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
-
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 __author__ = "David Boddie <david@boddie.org.uk>"
-__date__ = "Sun 8th June 2008"
-__version__ = "0.31"
+__date__ = "Sun 12th July 2008"
+__version__ = "0.40"
+__license__ = "GNU General Public License (version 3)"
 
 
 import os, string, struct, time
